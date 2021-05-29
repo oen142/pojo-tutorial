789242fca7bf79b9e3fd6c24cc856e4cb41eea4f
oen142 <oen142@naver.com>
2021-05-29 23:07:42 +0900

    Initial commit

---
+++ src/main/java/com/wani/pojotutorial/PojoTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.pojotutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojoTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PojoTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/pojotutorial/PojoTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.pojotutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojoTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
